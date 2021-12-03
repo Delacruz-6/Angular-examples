fee5715fb13a44d7a99f330fd5d079f79c4f1695
Delacruz-6 <gdlcruzguzman@gmail.com>
2021-12-03 08:05:57 +0100

    Subido ejercicio con firebase

---
+++ angular-gasApp-firebase/src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  firebase: {
+    projectId: 'gasapp-6e386',
+    appId: '1:686433861699:web:70008673cfae7ced5794f6',
+    storageBucket: 'gasapp-6e386.appspot.com',
+    apiKey: 'AIzaSyCUFTtj-yF5t1mgPCTuYZrS1dRE5AM7JtU',
+    authDomain: 'gasapp-6e386.firebaseapp.com',
+    messagingSenderId: '686433861699',
+    measurementId: 'G-W1NVSYHWZB',
+  },
+  production: true
+};
---
+++ angular-gasApp-firebase/src/environments/environment.ts
@@ -0,0 +1,34 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  firebase: {
+    projectId: 'gasapp-6e386',
+    appId: '1:686433861699:web:70008673cfae7ced5794f6',
+    storageBucket: 'gasapp-6e386.appspot.com',
+    apiKey: 'AIzaSyCUFTtj-yF5t1mgPCTuYZrS1dRE5AM7JtU',
+    authDomain: 'gasapp-6e386.firebaseapp.com',
+    messagingSenderId: '686433861699',
+    measurementId: 'G-W1NVSYHWZB',
+  },
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyCUFTtj-yF5t1mgPCTuYZrS1dRE5AM7JtU",
+    authDomain: "gasapp-6e386.firebaseapp.com",
+    projectId: "gasapp-6e386",
+    storageBucket: "gasapp-6e386.appspot.com",
+    messagingSenderId: "686433861699",
+    appId: "1:686433861699:web:3758c9ff76d98c9d5794f6",
+    measurementId: "G-K8ST0G1NS4"
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
