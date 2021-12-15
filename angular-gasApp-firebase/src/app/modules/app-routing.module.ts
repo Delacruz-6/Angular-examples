710c301d72e346ce1c47fd2cd2ebd5af8ef25fb8
Delacruz-6 <gdlcruzguzman@gmail.com>
2021-12-15 12:20:13 +0100

    Añadido authGuard favoritas

--- angular-gasApp-firebase/src/app/modules/app-routing.module.ts
+++ angular-gasApp-firebase/src/app/modules/app-routing.module.ts
@@ -5,12 +5,15 @@ import { SignInComponent } from '../components/sign-in/sign-in.component';
 import { ListGasolineraComponent } from '../components/list-gasolinera/list-gasolinera.component';
 import { FavoritasGasolinerasComponent } from '../components/favoritas-gasolineras/favoritas-gasolineras.component';
 import { ListasGasComponent } from '../components/listas-gas/listas-gas.component';
+import { AngularFireAuthGuard, loggedIn } from '@angular/fire/compat/auth-guard';
+
+const loggedInV = () => loggedIn;
 
 
 const routes: Routes = [
   {path: '', pathMatch: 'full', component: ListGasolineraComponent},
   { path: 'sign-in',pathMatch: 'full', component: SignInComponent },
-  { path: 'favoritas',pathMatch: 'full', component: FavoritasGasolinerasComponent },
+  { path: 'favoritas',pathMatch: 'full', component: FavoritasGasolinerasComponent, canActivate: [AngularFireAuthGuard] , data: { authGuardPipe: loggedInV } },
   { path: 'listas', pathMatch: 'full', component: ListasGasComponent },
 ];
 
